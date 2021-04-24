6e3645fbf236a4954731b6d312cdb75b50980868
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-04-24 16:09:47 +0900

    feat: Complete js stone game in typescript

---
+++ ts_practice_js_stone/js_stone.js
@@ -0,0 +1,118 @@
+"use strict";
+// 영웅 클래스
+var Hero = /** @class */ (function () {
+    function Hero(mine) {
+        this.mine = mine;
+        this.att = Math.ceil(Math.random() * 2);
+        this.hp = Math.ceil(Math.random() * 5) + 25;
+        this.hero = true;
+        this.field = true;
+    }
+    return Hero;
+}());
+// 쫄병 클래스
+var Sub = /** @class */ (function () {
+    function Sub(mine) {
+        this.field = false;
+        this.mine = mine;
+        this.att = Math.ceil(Math.random() * 5);
+        this.hp = Math.ceil(Math.random() * 5);
+        this.cost = Math.floor((this.att + this.hp) / 2);
+    }
+    return Sub;
+}());
+var a = {
+    add: function (a, b) { return a + b; }
+};
+var b = {
+    add: function (a, b) { return a + b; }
+};
+console.log(a.add(1, 2));
+console.log(b.add('a', 'b'));
+var opponent = {
+    hero: document.getElementById('rival-hero'),
+    deck: document.getElementById('rival-deck'),
+    field: document.getElementById('rival-cards'),
+    cost: document.getElementById('rival-cost'),
+    deckData: [],
+    heroData: null,
+    fieldData: [],
+    chosenCard: null,
+    chosenCardData: null
+};
+var me = {
+    hero: document.getElementById('rival-hero'),
+    deck: document.getElementById('rival-deck'),
+    field: document.getElementById('rival-cards'),
+    cost: document.getElementById('rival-cost'),
+    deckData: [],
+    heroData: null,
+    fieldData: [],
+    chosenCard: null,
+    chosenCardData: null
+};
+var turnButton = document.getElementById('turn-btn');
+var turn = true; // true면 내 턴, false면 상대 턴
+function initiate() {
+    [opponent, me].forEach(function (item) {
+        item.deckData = [];
+        item.heroData = null;
+        item.fieldData = [];
+        item.chosenCard = null;
+        item.chosenCardData = null;
+    });
+    // 객체를 생성할때 개별인자로 직접 전달하지 않고, 객체로써 넘겨주도록 하자. 
+    createDeck({ mine: false, count: 5 });
+    createDeck({ mine: false, count: 5 });
+    createHero({ mine: false });
+    createHero({ mine: true });
+    redrawScreen({ mine: true });
+    redrawScreen({ mine: false });
+}
+initiate();
+function createDeck(_a) {
+    var mine = _a.mine, count = _a.count;
+    var player = mine ? me : opponent;
+    for (var i = 0; i < count; i++) {
+        player.deckData.push(new Sub(mine));
+    }
+    ;
+    redrawDeck(player);
+}
+function createHero(_a) {
+    var mine = _a.mine;
+    var player = mine ? me : opponent;
+    player.heroData = new Hero(mine);
+    connectCardDOM({ data: player.heroData, DOM: player.hero, hero: true });
+}
+function connectCardDOM(_a) {
+    var data = _a.data, DOM = _a.DOM, _b = _a.hero, hero = _b === void 0 ? false : _b;
+    var cardEl = document.querySelector('.card-hidden .card').cloneNode(true);
+    // 무조건 HTML 요소가 존재한다고 보장 !
+    cardEl.querySelector('.card-att').textContent = String(data.att);
+    cardEl.querySelector('.card-hp').textContent = String(data.hp);
+    if (hero) {
+        cardEl.querySelector('.card-cost').style.display = 'none';
+        var name_1 = document.createElement('div');
+        cardEl.appendChild(name_1);
+    }
+    DOM.appendChild(cardEl);
+}
+function redrawHero(target) {
+    if (!target.heroData) {
+        throw new Error('heroData가 없습니다.');
+    }
+    target.hero.innerHTML = '';
+    connectCardDOM({ data: target.heroData, DOM: target.hero, hero: true });
+}
+function redrawScreen(_a) {
+    var mine = _a.mine;
+    var player = mine ? me : opponent;
+    redrawHero(player);
+}
+function redrawDeck(target) {
+    target.deck.innerHTML = '';
+    target.deckData.forEach(function (data) {
+        connectCardDOM({ data: data, DOM: target.deck });
+    });
+}
---
+++ ts_practice_js_stone/js_stone.ts
@@ -0,0 +1,172 @@
+interface Card {
+  att: number;
+  hp: number;
+  mine: boolean;
+  field: boolean;
+  cost?: number;
+  hero?: boolean;
+}
+
+// 영웅 클래스
+class Hero implements Card {
+  public att: number;
+  public hp: number;
+  public hero: boolean;
+  public field: true;
+  public mine: boolean;
+  constructor(mine: boolean){
+    this.mine = mine;
+    this.att = Math.ceil(Math.random() * 2);
+    this.hp = Math.ceil(Math.random() * 5) + 25;
+    this.hero = true;
+    this.field = true;
+  }
+}
+
+
+// 쫄병 클래스
+class Sub implements Card {
+  public att: number;
+  public hp: number;
+  public field: boolean = false;
+  public mine: boolean;
+  public cost: number;
+  constructor(mine: boolean){
+    this.mine = mine;
+    this.att = Math.ceil(Math.random() * 5);
+    this.hp = Math.ceil(Math.random() * 5);
+    this.cost = Math.floor((this.att + this.hp) / 2);
+  }
+}
+
+
+
+interface obj<T> {
+  add: (a: T, b: T) => T
+}
+
+const a: obj<number> = {
+  add: (a, b) => a + b,
+}
+
+const b: obj<string> = {
+  add: (a, b) => a + b,
+}
+
+console.log(a.add(1, 2));
+console.log(b.add('a', 'b'));
+
+interface Player {
+  hero: HTMLDivElement,
+  deck: HTMLDivElement,
+  field: HTMLDivElement,
+  cost: HTMLDivElement,
+  deckData: Card[],
+  heroData?: Card | null,
+  fieldData: Card[],
+  // 공격을 하기 위해서 내 카드를 선택하고 상대방의 카드를 선택해야 한다.
+  // 따라서 null일 수 있다는 가정을 해야 한다.
+  chosenCard?: HTMLDivElement | null,
+  chosenCardData?: Card | null
+}
+
+const opponent: Player = {
+  hero: document.getElementById('rival-hero') as HTMLDivElement,
+  deck: document.getElementById('rival-deck') as HTMLDivElement,
+  field: document.getElementById('rival-cards') as HTMLDivElement,
+  cost: document.getElementById('rival-cost') as HTMLDivElement,
+  deckData: [],
+  heroData: null,
+  fieldData: [],
+  chosenCard: null,
+  chosenCardData: null,
+};
+
+
+
+const me: Player = {
+  hero: document.getElementById('rival-hero') as HTMLDivElement,
+  deck: document.getElementById('rival-deck') as HTMLDivElement,
+  field: document.getElementById('rival-cards') as HTMLDivElement,
+  cost: document.getElementById('rival-cost') as HTMLDivElement,
+  deckData: [],
+  heroData: null,
+  fieldData: [],
+  chosenCard: null,
+  chosenCardData: null,
+};
+
+const turnButton = document.getElementById('turn-btn') as HTMLButtonElement;
+let turn = true; // true면 내 턴, false면 상대 턴
+
+function initiate(){
+  [opponent, me].forEach((item) => {
+    item.deckData = [];
+    item.heroData = null;
+    item.fieldData = [];
+    item.chosenCard = null;
+    item.chosenCardData = null;
+  });
+  // 객체를 생성할때 개별인자로 직접 전달하지 않고, 객체로써 넘겨주도록 하자. 
+  createDeck({mine: false, count: 5});
+  createDeck({mine: false, count: 5});
+  createHero({mine: false});
+  createHero({mine: true});
+  redrawScreen({mine: true});
+  redrawScreen({mine: false});
+}
+
+initiate();
+
+function createDeck({ mine, count }: { mine: boolean, count: number}){
+  const player = mine ? me : opponent;
+  for(let i: number = 0; i < count; i++){
+    player.deckData.push(new Sub(mine));
+  };
+  redrawDeck(player);
+}
+
+function createHero({ mine }: { mine: boolean }){
+  const player = mine ? me : opponent;
+  player.heroData = new Hero(mine);
+  connectCardDOM({ data: player.heroData, DOM: player.hero, hero: true});
+}
+
+interface A {
+  data: Card,
+  DOM: HTMLDivElement,
+  hero?: boolean
+}
+
+function connectCardDOM({ data, DOM, hero = false }: A ){
+  const cardEl = document.querySelector('.card-hidden .card')!.cloneNode(true) as HTMLDivElement;
+  // 무조건 HTML 요소가 존재한다고 보장 !
+  cardEl.querySelector('.card-att')!.textContent = String(data.att);
+  cardEl.querySelector('.card-hp')!.textContent = String(data.hp);
+  if (hero){
+    (cardEl.querySelector('.card-cost') as HTMLDivElement).style.display = 'none';
+    const name = document.createElement('div');
+    cardEl.appendChild(name);
+  }
+  DOM.appendChild(cardEl);
+}
+
+function redrawHero(target: Player){
+  if(!target.heroData){
+    throw new Error('heroData가 없습니다.');
+  }
+  target.hero.innerHTML = '';
+  connectCardDOM({ data: target.heroData, DOM: target.hero, hero: true });
+}
+
+function redrawScreen({ mine }: { mine: boolean }) {
+  const player = mine ? me : opponent;
+  redrawHero(player);
+}
+
+function redrawDeck(target: Player){
+  target.deck.innerHTML = '';
+  target.deckData.forEach((data) => {
+    connectCardDOM({ data, DOM: target.deck });
+  });
+}
